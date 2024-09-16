7e20daef2a9070eb6961ab2ba3ed8b949d56446d
Sundram11 <yadavsundram123@gmail.com>
2024-09-16 08:07:18 +0530

    add cors cookies-parser apierror-handling api-response-std and set-limit-of-data-middleware-app.use -> .json,urlencoded,.static

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import app from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
 
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : 
+            ${ process.env.PORT }`);
+    })
+    app.on("error:", ()=>{
+        console.log("ERROR", error);
+        throw error
+    })
+}).catch((error) =>{
+    console.log("MONGODB CONNECTION FAILED",error);
+});
 
