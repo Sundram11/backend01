57e110e6c50c16ecb3501798e76acfc6e610062f
Sundram11 <yadavsundram123@gmail.com>
2024-09-21 07:30:49 +0530

    subscription model add

--- src/routes/user.routs.js
+++ src/routes/user.routs.js
@@ -23,4 +23,5 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refres-token").post(refreshAccessToken)
+
 export default router;
